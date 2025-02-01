09b57e07070a5612379d9538796d960b9d49b021
Kazuma Sakaguchi <sakaguchi@KazumanoMacBook-Pro.local>
2025-02-01 12:29:10 +0900

    tradeモデル実装

---
+++ backend/domain/model/trade.go
@@ -0,0 +1,70 @@
+package model
+
+import (
+	"errors"
+	"math"
+)
+
+type TradeSide string
+
+const (
+	Sell TradeSide = "sell"
+	Buy  TradeSide = "buy"
+)
+
+type Trade struct {
+	Side                 TradeSide
+	HigherTimeFrameRates Rates
+	LowerTimeFrameRates  Rates
+	Quantity             float64
+}
+
+func NewTrade(higherTimeFrameRates, lowerTimeFrameRates Rates) (*Trade, error) {
+	if len(higherTimeFrameRates) == 0 {
+		return nil, errors.New("higher timeframe rates cannot be empty")
+	}
+	if len(lowerTimeFrameRates) == 0 {
+		return nil, errors.New("lower timeframe rates cannot be empty")
+	}
+
+	return &Trade{
+		HigherTimeFrameRates: higherTimeFrameRates,
+		LowerTimeFrameRates:  lowerTimeFrameRates,
+	}, nil
+}
+
+func (trade *Trade) Tradable() bool {
+	if trade.isTrending() && trade.isPossibleEntry() {
+		return true
+	}
+
+	return false
+}
+
+// 上位足の相場を見てトレンドが発生しているか判定する
+func (trade *Trade) isTrending() bool {
+	// TODO: implements
+	trade.Side = Sell
+	return false
+}
+
+// 下位足の相場を見てエントリー可能か判定する
+func (trade *Trade) isPossibleEntry() bool {
+	// TODO: implements
+	return false
+}
+
+// 総資産から取引額の計算
+func (trade *Trade) CalculateQuantity(totalWallet float64) error {
+	if totalWallet < 100.0 {
+		return errors.New("totalWallet must be greater than 100.0USD")
+	}
+
+	trade.Quantity = truncateToOneDecimal(totalWallet * 0.1)
+
+	return nil
+}
+
+func truncateToOneDecimal(value float64) float64 {
+	return math.Floor(value*10) / 10
+}
